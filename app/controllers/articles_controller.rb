ca7abba63476e5c606379a5834bc8d9469271afc
SamTreon <stat123@hotmail.co.uk>
2023-02-21 17:09:15 +0000

    all the backend done, with some HTML and CSS styling

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(articles_params)
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # No need for app/views/articles/destroy.html.erb
+    redirect_to articles_path, status: :see_other
+  end
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
